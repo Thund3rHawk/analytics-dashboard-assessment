23e31c345113b08a8adc9870f06d1d1205ff293d
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-04-20 11:19:11 +0530

    chore: update import

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,7 +9,8 @@ import ElectricDistributionCard from "@/components/shared/ElectricDistributionCa
 import VehicleDistributionCard from "@/components/shared/VehicleDistributionCard";
 import DetailsCard from "@/components/shared/DetailsCard";
 import LoadingPage from "./LoadingPage";
-import { Vehicle } from "../types/index.ts";
+import { Vehicle } from "@/types/index.ts";
+
 
 const Dashboard = () => {
   const [data, setData] = useState<Vehicle[]>([]);
